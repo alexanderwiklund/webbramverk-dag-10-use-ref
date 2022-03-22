7d66aed8f0ca17c42a4abb06058a55483cf724ee
Alexander Wiklund <alex@plexi.se>
2022-03-22 11:06:23 +0100

    useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const renderCount = useRef(0);
+  const previousValue = useRef();
+
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef();
+
+  const videoRef = useRef();
+
+  useEffect(() => {
+    console.log("App Rendered");
+    renderCount.current = renderCount.current + 1;
+    previousValue.current = count;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          ref={inputRef}
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        ></input>
+        <button onClick={() => inputRef.current.focus()}>FOCUS</button>
+      </div>
+
+      <div>
+        <div>{renderCount.current}</div>
+        <button onClick={() => setCount((previous) => previous + 1)}>
+          INCREASE COUNT
+        </button>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 8,
+        }}
+      >
+        <video
+          controls
+          ref={videoRef}
+          src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+          poster="https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217"
+          width="620"
+        ></video>
+        <button onClick={() => videoRef.current.play()}>PLAY</button>
+        <button onClick={() => videoRef.current.pause()}>PAUSE</button>
+      </div>
     </div>
   );
 }
